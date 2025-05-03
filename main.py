58df213334af4564f5c0bba2eaba6bc5106de3e2
wskowro <wskowro@gmail.com>
2025-05-03 12:07:40 -0400

    added clock and framerate of 60 to game screen

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
