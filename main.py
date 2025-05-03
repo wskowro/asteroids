9462aa4b49c72d20da98647386b789c2630242bd
wskowro <wskowro@gmail.com>
2025-05-03 12:45:39 -0400

    Added player rotation using the a and d key to the game

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
